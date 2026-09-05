25c03e90e0ffa26a87aa51e7c828a207c7f2707d
Christoph Herzog <chris@theduke.at>
2026-09-05 07:19:04 +0200

    test: add TSX CLI fixtures

---
+++ cli_tests/fixtures/tsx/src/namespaces.tsx
@@ -0,0 +1,8 @@
+export namespace Admin {
+  /** An administration header. */
+  export function Header() {
+    return <header>Admin</header>;
+  }
+
+  export const Footer = () => <footer>Admin</footer>;
+}
